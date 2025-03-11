f0c047471ab479ae5fb226fbc2d88de834d1388d
mponsetim <matias.ponseti1@estudiant.uib.cat>
2025-03-11 16:16:38 +0100

    feat: :sparkles: añadir subject al componente de partituras

--- public/js/event-bus.js
+++ public/js/event-bus.js
@@ -1,8 +1,12 @@
 $(function() {
     const { Subject } = rxjs;
-    const eventBus = new Subject();
+    const eventBus_piano = new Subject();
+    const eventBus_sheets = new Subject();
     
-    $('video-component')[0].subject = eventBus
-    $('piano-component')[0].subject = eventBus
+    $('video-component')[0].subjectPiano = eventBus_piano;
+    $('piano-component')[0].subject = eventBus_piano;
+
+    $('video-component')[0].subjectSheets = eventBus_sheets;
+    $('sheet-component')[0].subject = eventBus_sheets;
     
 });
