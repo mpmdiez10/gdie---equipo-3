d8bb2b5c75b73d3386529b7f3668922a54503802
mponsetim <matias.ponseti1@estudiant.uib.cat>
2025-04-21 11:32:52 +0200

    fix: :bug: add TODO comment for handling init mobile message in clone-socket.js

--- public/assets/js/clone-socket.js
+++ public/assets/js/clone-socket.js
@@ -4,7 +4,7 @@ const roomId = urlParams.get('roomId');
 const socket = io();
 socket.emit('init mobile message', {roomId: roomId});
 socket.on('init mobile message', (msg) => {
-  alert(msg.text);
+  alert(msg.text); //TODO: Mirar que habría que hacer aquí
 });
 socket.on('room message', (msg) => {
   piano = document.querySelector('piano-component');
