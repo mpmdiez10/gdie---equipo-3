d9cb8673f342252b23afe798ad07f6c293d314af
mponsetim <matias.ponseti1@estudiant.uib.cat>
2025-04-24 16:06:49 +0200

    fix: :bug: arreglar la funcionalidad de los mensajes

--- index.js
+++ index.js
@@ -15,27 +15,35 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
 
+    let socketRoomID;
+
     // Recibe el mensaje de inicialización del main y une el socket a la nueva habitación
     socket.on('init main message', () => {
+        // Generar un ID único para la habitación usando la fecha y hora actual
+        // Quitar la fecha hace que no funcione
+        const timestamp = new Date().getTime();
+        socketRoomID = `${socket.id.toString()}-${timestamp}`;
+
+
         // Crear habitacion y unir socket
-        socket.join(socket.id); // TODO: Descomentar para producción
-        console.log('main joined room:', socket.id); // TODO: Descomentar para producción
-        // socket.join('hola'); // TODO: Descomentar para desarrollo
+        socket.join(socketRoomID);
+        console.log('main joined room:', socketRoomID);
+        
         // Enviar código de habitación al main
-        socket.emit('init main message', socket.id);
+        socket.emit('init main message', socketRoomID);
     });
 
     // Recibe las notas del main y las envía a la habitación
     socket.on('main message', (msg) => {
-        socket.to(socket.id).emit('room message', {text: msg.keys}); // TODO: Descomentar para producción
-        // socket.to('hola').emit('room message', {text: msg.keys}); // TODO: Descomentar para desarrollo
+        socket.to(socketRoomID).emit('room message', {text: msg.keys});
     });
 
     // Recibe el mensaje de inicialización del clone y une el socket a la habitación
     socket.on('init clone message', (msg) => {
+        socketRoomID = msg.roomId;
         // Unir a habitación
-        socket.join(msg.roomId);
-        console.log('clone joined room:', msg.roomId);
+        socket.join(socketRoomID);
+        console.log('clone joined room:', socketRoomID);
     });
 
     // Recibe la acción sobre el video del clone y la envía a la habitación
